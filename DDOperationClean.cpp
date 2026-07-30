c1bb4ff31ce7c08fe6d76618f04d34a8be59a339
bartle <sourcetree@chrisbartle.com>
2026-07-30 11:52:12 -0600

    Cleaning now checks any file that's not in the manifest to confirm that it was the correct prefix.

--- DDOperationClean.cpp
+++ DDOperationClean.cpp
@@ -1,4 +1,5 @@
 #include "DDOperationClean.h"
+#include <fstream>
 
 using namespace std;
 
@@ -21,23 +22,32 @@ void DDOperationClean::ChildDoOperation(DDParameters &parameters)
     //remove all directories and files that match the DD_* pattern
     //vector<DDFile> fileQueue;
     vector<filesystem::path> directoriesToDeleteLater;
+    std::vector<std::unique_ptr<DDFile>> filesToDelete;
     for (auto it = filesystem::recursive_directory_iterator(parameters.getAbsoluteDirectoryPath()); it != filesystem::recursive_directory_iterator(); ++it)
     {
         const auto& entry = *it;
         //Delete any files that start with DD_
         if (filesystem::is_regular_file(entry.path()) && entry.path().filename().string().starts_with("DD_"))
         {
-            filesystem::remove(entry.path());
-            m_processedCount++;
+            filesToDelete.emplace_back(std::make_unique<DDFile>());
+            DDFile& file = *(filesToDelete.back());
+            file.setRelativePathname(entry.path());
+            file.setProcessingStatus(DDFile::QUEUED);
+            DoFileOperation(file, parameters, 0, 1);
+            m_targetCount++;
         }
         else if (filesystem::is_directory(entry.path()))
         {
             if (entry.path().filename().string().starts_with("DD_"))
+            {
                 directoriesToDeleteLater.push_back(entry.path());
+                m_targetCount++;
+            }
             else
                 it.disable_recursion_pending();
         }
     }
+    WaitForThreadsToComplete();
     //Iterate backwards through all of the directories we found and remove them one by one
     for (auto it = directoriesToDeleteLater.rbegin(); it != directoriesToDeleteLater.rend(); ++it)
     {
@@ -73,24 +83,39 @@ void DDOperationClean::ChildDoFileOperation(DDFile &file, DDParameters &paramete
     if (file.processingStatus() != DDFile::QUEUED)
         return;
 
-    filesystem::path absolutePathname = parameters.ConvertToAbsolutePath(file.relativePathname());
-
     //Mark that this file is being processed
     file.setProcessingStatus(DDFile::STARTED);
     try
     {
+        filesystem::path absolutePathname;
+        //Special processing. If size = 1 then that's an indicator that this file is not part of the manifest.
+        if (size == 1)
+        {
+            absolutePathname = file.relativePathname();
+            //We need to confirm that this is indeed a DummyDirectory file. We do this by opening it and confirming
+            //the file's prefix
+            int prefixLength = m_filePrefix.length();
+            std::ifstream inFile(absolutePathname, std::ios::binary);
+            std::vector<char> buffer(prefixLength);
+            inFile.read(buffer.data(), prefixLength);
+            if ((inFile.gcount() != prefixLength) || !std::equal(m_filePrefix.begin(), m_filePrefix.end(), buffer.begin()))
+                return;
+        }
+        else
+            absolutePathname = parameters.ConvertToAbsolutePath(file.relativePathname());
+
         //Delete the file
         filesystem::remove(absolutePathname);
         file.setProcessingStatus(DDFile::DELETED);
         m_processedCount++;
     }
     catch (const std::exception& e) {
-        file.recordProcessingError("Exception thrown when processing file " + absolutePathname.string() + ": " + e.what());
+        file.recordProcessingError("Exception thrown when processing file " + file.relativePathname().string() + ": " + e.what());
         return;
     }
     catch(...)
     {
-        file.recordProcessingError("Unknown exception when processing file " + absolutePathname.string());
+        file.recordProcessingError("Unknown exception when processing file " + file.relativePathname().string());
         return;
     }
 }
